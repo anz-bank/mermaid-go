938cbaf8b55c2eb7cd1d7627199aff35f316349b
joshcarp <joshuacarpeggiani@gmail.com>
2020-04-02 16:32:57 +1100

    Rename module to mermaid-go

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/joshcarp/sysl-mermaid
+module github.com/joshcarp/mermaid-go
 
 go 1.14
 
--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/spf13/afero"
 
-	"github.com/joshcarp/sysl-mermaid/mermaid"
+	"github.com/joshcarp/mermaid-go/mermaid"
 )
 
 func main() {
